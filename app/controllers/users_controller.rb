1dd6051b4d50cebf87c55082008886a8ecc93690
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-08-20 14:09:39 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+  def show
+  end
+  def create
+    # binding.pry
+    @user = User.new(user_params)
+    # @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:password,:password_confirmation)
+  end
+end
