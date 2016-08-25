edb2a494e92ab403857b8cc5577a645db8536e73
Raycent <raycent.tan@flatironschool.com>
2016-08-25 15:47:30 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to root_path
+    end
+  end
+end
