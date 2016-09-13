e598237fae115d749f3d6a14e3e051840b76bfa9
satub <satu.barnhill@gmail.com>
2016-09-13 11:46:15 -0400

    Basic webpage functionality added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,21 @@
 class SessionsController < ApplicationController
   def new
-
   end
+  
   def create
    @user = User.find_by(name: params[:user][:name])
-   return head(:forbidden) unless @user.authenticate(params[:user][:password])
-   session[:user_id] = @user.id
-   redirect_to '/welcome'
+   if @user.authenticate(params[:user][:password])
+     session[:user_id] = @user.id
+     redirect_to '/welcome'
+   else
+     flash[:message] = "Password or username does not match! :o No soup for you!"
+     redirect_to '/login'
+   end
  end
 
+  def destroy
+    session[:user_id] = nil unless session[:user_id].nil?
+    flash[:message] = "You have been succesfully logged out."
+    redirect_to '/login'
+  end
 end
