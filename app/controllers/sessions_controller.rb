2a66337b67af35afe7d7ebf9dbd9c6b503023d5e
ozPop <ozziepopovas@gmail.com>
2016-09-27 16:48:21 -0400

    refactor session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,28 @@
 class SessionsController < ApplicationController
-
   def login
     render :'public/login'
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    return head(:forbidden) unless @user.authenticate(params[:user][:password])
-    session[:user_id] = @user.id
-    redirect_to user_index_path
+    case @user = User.find_by(name: user_params[:name])
+    when nil
+      redirect_to login_path
+    when @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_index_path
+    else
+      render :'public/login', status: 403
+    end
   end
 
   def destroy
     session.delete :user_id
     redirect_to login_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 end
