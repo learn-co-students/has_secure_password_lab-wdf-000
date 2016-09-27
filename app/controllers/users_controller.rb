9af532d4d3b35ef6b056f969dccb4986be7ec3f9
ozPop <ozziepopovas@gmail.com>
2016-09-27 14:28:33 -0400

    update routes, add users_controller, new view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
