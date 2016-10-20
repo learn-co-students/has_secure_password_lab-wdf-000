9c73e9a62cecadd0a5326b932aa5ff4d097a0342
Kelene Chung <simoy93@gmail.com>
2016-10-20 13:25:40 -0400

    DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  resources :users
+  # resources :sessions
+
+  get '/signup' => 'users#new'
+  get '/login' =>  'sessions#new'
+  post '/login' => 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
